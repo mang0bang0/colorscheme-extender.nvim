68f317516284bb0dc9c4ca3100450cdcf977631d
mango <mangogit@protonmail.com>
2024-01-22 13:26:04 -0500

    refactor start function into smaller functions

--- lua/colorscheme-extender/init.lua
+++ lua/colorscheme-extender/init.lua
@@ -15,106 +15,11 @@ M._demoWinNum = 0
 -- The buffer and window number of the I/O buffer
 M._ioBufNum = 0
 M._ioWinNum = 0
--- The text displayed in the demo buffer
-M._text = ""
--- The indents to the left of the demo buffer
-M._indent = 0
 -- The namespace ID of the extmarks used for highlighting in the demo
 -- buffer
 M._nsid = 0
 
-function M._setDemoBuffer(text, indent)
-    -- Move to the demo window
-    vim.api.nvim_set_current_win(M._demoWinNum)
-
-    -- Make sure the buffer is modifiable
-    vim.opt_local.modifiable = true
-    vim.opt_local.readonly = false
-
-    -- Clear the highlights in the demo buffer
-    vim.api.nvim_buf_clear_namespace(0, 0, 0, -1)
-
-    -- Clear the demo buffer
-    vim.api.nvim_buf_set_lines(M._demoBufNum, 0, -1, false, {})
-
-    -- Set each line to be 80 chars wide
-    local lineWidth = 80
-
-    -- -5 so that we have some space on the very left
-    local entriesPerLine = math.floor((lineWidth + 1 - indent) / (string.len(text) + 1))
-    local startingLineIndex = 0
-
-    -- For each table, fg, bg, and fgBg
-    for _,colors in ipairs(M._colors) do
-        -- Get number of lines, and handle if there's a partial line in the end
-        -- Add 1 because we want empty lines between fg, bg, fgBg
-        local numOfLines = math.floor(#colors / entriesPerLine) + 1
-        local partial = false
-
-        if #colors % entriesPerLine > 0 then
-            partial = true
-            numOfLines = numOfLines + 1
-        end
-
-        -- Construct the text of a full line
-        local lineText = util._constructLine(text, indent, entriesPerLine)
-
-        -- Populate an array with the guarenteed full lines
-        local lines = {}
-
-        -- "- 2" because numOfLines includes the not guarenteed full line and an
-        -- empty line
-        for _ = 1, numOfLines - 2 do
-            table.insert(lines, lineText)
-        end
-
-        -- If there's a partial line, construct it
-        if partial then
-            lineText = util._constructLine(text, indent, #colors % entriesPerLine)
-        end
-
-        -- Add the last line
-        table.insert(lines, lineText)
-
-        -- Then add an empty line
-        table.insert(lines, "")
-
-        -- Set the buffer
-        vim.api.nvim_buf_set_lines(
-            M._demoBufNum,
-            startingLineIndex,
-            startingLineIndex + numOfLines,
-            false,
-            lines
-        )
-
-        -- Add highlights
-        for i,val in ipairs(colors) do
-            vim.api.nvim_buf_add_highlight(
-                M._demoBufNum,
-                -1,
-                val.name,
-                math.floor((i - 1) / entriesPerLine + startingLineIndex),
-                indent + (i - 1) % entriesPerLine * (string.len(text) + 1),
-                indent + (i - 1) % entriesPerLine * (string.len(text) + 1) +
-                    string.len(text)
-            )
-        end
-
-        startingLineIndex = startingLineIndex + numOfLines
-    end
-
-    -- Set the buffer to no longer be modifiable
-    vim.opt_local.modifiable = false
-    vim.opt_local.readonly = true
-end
-
--- Testing functions to see as we go
-function M.start(textInput, indentInput, pattern)
-    -- Set global variables based on user input
-    M._text = textInput
-    M._indent = indentInput
-
+function M._getHighlights(pattern)
     -- Get a table of the highlights of the current colorscheme
     M._highlights = vim.api.nvim_get_hl(0, {})
 
@@ -164,9 +69,102 @@ function M.start(textInput, indentInput, pattern)
     -- Get the buffer number of the I/O buffer
     M._ioBufNum = vim.api.nvim_get_current_buf()
     M._ioWinNum = vim.api.nvim_get_current_win()
+end
 
+function M._setDemoBuffer(text, indent)
+    -- Move to the demo window
+    vim.api.nvim_set_current_win(M._demoWinNum)
+
+    -- Make sure the buffer is modifiable
+    vim.opt_local.modifiable = true
+    vim.opt_local.readonly = false
+
+    -- Clear the highlights in the demo buffer
+    vim.api.nvim_buf_clear_namespace(0, 0, 0, -1)
+
+    -- Clear the demo buffer
+    vim.api.nvim_buf_set_lines(M._demoBufNum, 0, -1, false, {})
+
+    -- Set each line to be 80 chars wide
+    local lineWidth = 80
+
+    -- -5 so that we have some space on the very left
+    local entriesPerLine = math.floor((lineWidth + 1 - indent) /
+                                      (string.len(text) + 1))
+    local startingLineIndex = 0
+
+    -- For each table, fg, bg, and fgBg
+    for _,colors in ipairs(M._colors) do
+        -- Get number of lines, and handle if there's a partial line in the end
+        -- Add 1 because we want empty lines between fg, bg, fgBg
+        local numOfLines = math.floor(#colors / entriesPerLine) + 1
+        local partial = false
+
+        if #colors % entriesPerLine > 0 then
+            partial = true
+            numOfLines = numOfLines + 1
+        end
+
+        -- Construct the text of a full line
+        local lineText = util._constructLine(text, indent, entriesPerLine)
+
+        -- Populate an array with the guarenteed full lines
+        local lines = {}
+
+        -- "- 2" because numOfLines includes the not guarenteed full line and an
+        -- empty line
+        for _ = 1, numOfLines - 2 do
+            table.insert(lines, lineText)
+        end
+
+        -- If there's a partial line, construct it
+        if partial then
+            lineText = util._constructLine(text, indent,
+                                           #colors % entriesPerLine)
+        end
+
+        -- Add the last line
+        table.insert(lines, lineText)
+
+        -- Then add an empty line
+        table.insert(lines, "")
+
+        -- Set the buffer
+        vim.api.nvim_buf_set_lines(
+            M._demoBufNum,
+            startingLineIndex,
+            startingLineIndex + numOfLines,
+            false,
+            lines
+        )
+
+        -- Add highlights
+        for i,val in ipairs(colors) do
+            vim.api.nvim_buf_add_highlight(
+                M._demoBufNum,
+                -1,
+                val.name,
+                math.floor((i - 1) / entriesPerLine + startingLineIndex),
+                indent + (i - 1) % entriesPerLine * (string.len(text) + 1),
+                indent + (i - 1) % entriesPerLine * (string.len(text) + 1) +
+                    string.len(text)
+            )
+        end
+
+        startingLineIndex = startingLineIndex + numOfLines
+    end
+
+    -- Set the buffer to no longer be modifiable
+    vim.opt_local.modifiable = false
+    vim.opt_local.readonly = true
+end
+
+-- Testing functions to see as we go
+function M.start(text, indent, pattern)
+    -- Get and categorize all the highlight groups
+    M._getHighlights(pattern)
     -- Add the texts and highlights to the tab
-    M._setDemoBuffer(M._text, M._indent)
+    M._setDemoBuffer(text, indent)
 end
 
 function M.getColorUnderCursor()
@@ -178,7 +176,8 @@ end
 function M.setup()
     vim.api.nvim_create_user_command(
         "ColorschemeExtend",
-        function () require("colorscheme-extender").start("gamba", 2, "^DevIcon") end,
+        function () require("colorscheme-extender").start("hello", 2,
+                                                          "^DevIcon") end,
         {}
     )
 end
