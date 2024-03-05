e992d0f3b67310882083132261b785764488862f
mango <mangogit@protonmail.com>
2024-03-04 21:48:45 -0500

    commit so I can work on desktop

--- lua/colorscheme-extender/init.lua
+++ lua/colorscheme-extender/init.lua
@@ -6,6 +6,12 @@ local util = require("colorscheme-extender.util")
 -- Table to hold highlights from nvim_get_hl()
 M._highlights = {}
 
+-- Module-level vars to hold text, indent, regex pattern, linewidth
+M._text = nil
+M._indent = nil
+M._pattern = nil
+M._lineWidth = nil
+
 -- Tables to hold colors. Value is the RRGGBB value
 -- Index 1 is fg, 2 is bg, 3 is fgbg. Only indices and not keys are used for
 -- table traversal
@@ -17,9 +23,13 @@ M._demoWinNum = 0
 -- The buffer and window number of the I/O buffer
 M._ioBufNum = 0
 M._ioWinNum = 0
--- The namespace ID of the extmarks used for highlighting in the demo buffer
-M._nsid = 0
+-- The namespace ID used by the plugin in a table
+M._nsid = {
+    hl = nil,
+    io = nil
+}
 
+-- TOOD: Set filetype of the demo buffer and IO buffer
 function M._createTab()
     -- Create a new tab page to work with
     vim.cmd.tabnew()
@@ -39,9 +49,9 @@ end
 
 -- Sets the demo buffer with indents and text highlighted with all
 -- unique highlights in the colorscheme
-function M._setDemoBuffer(text, indent)
+function M._setDemoBuffer(text, indent, lineWidth)
     -- Create a namespace for our highlights and record its nsid
-    M._nsid = vim.api.nvim_create_namespace("colorscheme-extender")
+    M._nsid.hl = vim.api.nvim_create_namespace("colex-hl")
 
     -- Move to the demo window
     vim.api.nvim_set_current_win(M._demoWinNum)
@@ -60,7 +70,6 @@ function M._setDemoBuffer(text, indent)
     -- TODO: is there a space after the text line if the string is like 100
     -- chars long and there's only 1 each line
     -- local lineWidth = 0
-    local lineWidth = 80
 
     -- Leave space for indents
     local entriesPerLine = math.floor((lineWidth + 1 - indent) /
@@ -116,7 +125,7 @@ function M._setDemoBuffer(text, indent)
         for i,val in ipairs(colors) do
             vim.api.nvim_buf_set_extmark(
                 M._demoBufNum,
-                M._nsid,
+                M._nsid.hl,
                 math.floor((i - 1) / entriesPerLine + startingLineIndex),
                 indent + (i - 1) % entriesPerLine * (string.len(text) + 1),
                 {
@@ -131,15 +140,17 @@ function M._setDemoBuffer(text, indent)
     end
 
     -- Add instructions at the bottom
+    local instructions = {
+        "Press K (shift+k) to see the name and color code of the highlight",
+        "under your cursor"
+    }
+
     vim.api.nvim_buf_set_lines(
         M._demoBufNum,
         startingLineIndex,
-        startingLineIndex + 2,
+        startingLineIndex + #instructions,
         false,
-        {
-            "Press K (shift+k) to see the name and color code of the highlight",
-            "under your cursor"
-        }
+        instructions
     )
 
     -- Set the buffer to no longer be modifiable
@@ -147,24 +158,61 @@ function M._setDemoBuffer(text, indent)
     vim.opt_local.readonly = true
 end
 
-function M._setIOBuffer()
+-- TODO: Use extmarks to keep track of text changes
+
+-- NOTE: Is it better to have a keybind that acts like an apply button compared
+-- to hot reloading by observing real-time changes?
+
+-- NOTE: Or we can track changes only on InsertLeave, and if someone pasted
+-- text, then they will have to enter and leave insert mode as kind of a way to
+-- "apply" their changes
+
+-- NOTE: see set_lines() for how to delete lines
+
+-- TODO: Move text, indent, pattern, and the like to global, because we need to
+-- use a callback function, which would need global vars
+
+-- TODO: Floating windows that show generated code
+function M._setIOBuffer(text, indent, lineWidth, pattern)
     -- Move to the IO window
     vim.api.nvim_set_current_win(M._ioWinNum)
 
-    -- Control flow:
-    -- Subscribe to changes the buffer, and record what text has been changed
-    -- and which line number has been changed
-    -- Set up an TextChanged autocmd. On callback, check what has been changed
-    -- and perform actions on the demo buffer
+    -- Do initial formatting
+    local bufferText = {
+        "Display Text (in one line):",
+        text,
+        "----------",
+        "Indent Length (positive number only):",
+        tostring(indent),
+        "----------",
+        "Line Width (positive number only):",
+        tostring(lineWidth),
+        "----------",
+        "Vim Regex Pattern to Filter Out:",
+        pattern ~= nil and pattern or "",
+        "----------",
+        "Bookmarks:"
+    }
 
-    -- Subscribe to changes in this buffer
+    vim.api.nvim_buf_set_lines(
+        M._ioBufNum,
+        0,
+        #bufferText,
+        false,
+        bufferText
+    )
 
+    -- Set extmarks for each of the input fields
+    -- First create a namespace for each so we can differentiate
+    M._nsid.io = vim.api.nvim_create_namespace("colex-io")
+    -- Then put an extmark on each input line
 
-    --TODO: floating windows as UI, compared to editing the buffer directly?
-    --      or we have one line say "word" and the line under takes input
-
-    -- TODO: have a list of "bookmarked colors" in the IO buffer, where the
-    -- user can add from the demo buffer and scroll the cursors through
+    -- NOTE: THIS FOLLOWING PARAGRAPH SHOULD BE IN A DIFFERENT FUNCTION
+    -- After every insert leave, we check the position of all the extmarks in IO
+    -- buffer to make sure that they stay on the correct lines, meaning that the
+    -- user didn't add multi-line inputs, which are forbidden
+    -- If it's good, then we proceed, otherwise we restore the IO buffer to the
+    -- most recent state (refilling the text, indent, etc.)
 
     -- TODO: for bookmarking fgbg, pick either only fg, only bg, and both?
 
@@ -177,7 +225,7 @@ function M.getColorUnderCursor()
     -- Technically the function is only called through a buffer-local binding,
     -- so this will only activate if the user calls it on their own. It's fine,
     -- but they probably won't get a lot of useful information, as this only
-    -- works on extmarks
+    -- works on extmarks and not treesitter
     if vim.api.nvim_get_current_win() ~= M._demoWinNum then
         print("You are not in the demo buffer!")
     end
@@ -194,8 +242,8 @@ function M.getColorUnderCursor()
                                         semantic_tokens = false
                                     })
 
-    -- Check if the extmarks table is empty with next() (table would not be nil)
-    -- If it is empty, then it means there are no extmakrs (whitespace)
+    -- Check if the extmarks table is empty with next() (empty table isn't nil)
+    -- If it is empty, then it means there are no extmarks (whitespace)
     if next(inspect.extmarks) ~= nil then
         local highlight = inspect.extmarks[1].opts.hl_group
         local pString = "hl_group name: " .. highlight
@@ -221,7 +269,13 @@ function M.getColorUnderCursor()
 end
 
 -- Testing functions to see as we go
-function M.start(text, indent, pattern)
+function M.start(text, indent, lineWidth, pattern)
+    -- Store settings into module-level variables
+    M._text = text
+    M._indent = indent
+    M._lineWidth = lineWidth
+    M._pattern = pattern
+
     -- Get and categorize all the highlight groups
     M._highlights, M._colors = util._getHighlights(pattern)
 
@@ -229,10 +283,10 @@ function M.start(text, indent, pattern)
     M._createTab()
 
     -- Add the texts and highlights to the tab
-    M._setDemoBuffer(text, indent)
+    M._setDemoBuffer(text, indent, lineWidth)
 
     -- Set up the UI in the IO buffer
-    M._setIOBuffer()
+    M._setIOBuffer(text, indent, lineWidth, pattern)
 
     -- Set a local keymap for getting the color codes of higihlights in the demo
     -- buffer
@@ -242,20 +296,6 @@ function M.start(text, indent, pattern)
         function() require("colorscheme-extender").getColorUnderCursor() end,
         {silent = true, buffer = M._demoBufNum}
     )
-
-    -- Testing nvim_buf_attach
-    local online = function(_, _, _, firstline, _, _)
-        local testtext = vim.api.nvim_buf_get_lines(M._ioBufNum,
-                                                firstline,
-                                                firstline + 1,
-                                                false)
-
-        M._setDemoBuffer(testtext, 2)
-    end
-
-    vim.api.nvim_buf_attach(M._ioBufNum,
-                            false,
-                            {on_lines = online})
 end
 
 -- On setup, create a new command for users to call
